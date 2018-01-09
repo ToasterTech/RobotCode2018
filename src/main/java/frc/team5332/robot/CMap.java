db16c2fd9af1703ed85fcc734bfc30ffef135386
Eric Dunbar <eric@dunbar.net>
2018-01-08 20:53:47 -0500

    I got it i got it I got it

--- src/main/java/frc/team5332/robot/CMap.java
+++ src/main/java/frc/team5332/robot/CMap.java
@@ -1,6 +1,7 @@
 package main.java.frc.team5332.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class CMap {
     //Input Devices - Joysticks
@@ -11,6 +12,11 @@ public class CMap {
     //Input Devices - Buttons
 
     //PWM Motor Ports
+    public static int leftMotor1 = 0;
+    public static int leftMotor2 = 1;
+
+    public static int rightMotor1 = 2;
+    public static int rightMotor2 = 3;
 
     //DIO Ports
 
--- src/main/java/frc/team5332/subsystems/Drivetrain.java
+++ src/main/java/frc/team5332/subsystems/Drivetrain.java
@@ -1,14 +1,22 @@
 package main.java.frc.team5332.subsystems;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import main.java.frc.team5332.robot.CMap;
 
 public class Drivetrain extends Subsystem {
-    public SpeedControllerGroup test = new SpeedControllerGroup();
+    public SpeedControllerGroup leftDrive = new SpeedControllerGroup(new VictorSP(CMap.leftMotor1), new VictorSP(CMap.leftMotor2));
+    public SpeedControllerGroup rightDrive = new SpeedControllerGroup(new VictorSP(CMap.rightMotor1), new VictorSP(CMap.rightMotor2));
 
 
     public Drivetrain() {
         super();
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
     }
 
     public void tankDrive(){
