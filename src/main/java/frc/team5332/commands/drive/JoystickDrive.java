5a9643a5f68aa81c1e029358bd014298a31bdceb
Jonathan Daniel <gcagrad19@gmail.com>
2018-01-13 16:56:52 -0500

    Updated Gradle

--- src/main/java/frc/team5332/commands/drive/JoystickDrive.java
+++ src/main/java/frc/team5332/commands/drive/JoystickDrive.java
@@ -7,14 +7,14 @@ import main.java.frc.team5332.subsystems.Drivetrain;
 public class JoystickDrive extends Command {
 
     /**
-     *
+     * This command drives the drivetrain based on the joystick input.
      */
     public JoystickDrive(){
         requires(CMap.drive);
     }
 
     public void execute(){
-        CMap.drive.tankDrive(CMap.gamepad.getRawAxis(CMap.leftYAXis), CMap.gamepad.getRawAxis(CMap.rightYAxis));
+        CMap.drive.tankDrive(-CMap.gamepad.getRawAxis(CMap.leftYAXis), -CMap.gamepad.getRawAxis(CMap.rightYAxis));
     }
 
     @Override
