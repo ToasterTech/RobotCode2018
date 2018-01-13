43d0092f46ba96fbb3c3005cec66b913c4869e04
Jonathan Daniel <gcagrad19@gmail.com>
2018-01-13 14:20:19 -0500

    Added Limit Switch

--- src/main/java/frc/team5332/subsystems/Intake.java
+++ src/main/java/frc/team5332/subsystems/Intake.java
@@ -1,8 +1,15 @@
 package main.java.frc.team5332.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import main.java.frc.team5332.robot.CMap;
 
 public class Intake extends Subsystem {
+    DigitalInput limitSwitch;
+
+    public Intake(){
+        limitSwitch = new DigitalInput(CMap.intakeLimitSwitch);
+    }
 
     protected void initDefaultCommand(){
 
@@ -10,5 +17,9 @@ public class Intake extends Subsystem {
 
     public void intakeCube(){
 
+    }
+
+    public boolean cubeIsIn(){
+        return limitSwitch.get();
     }
 }
